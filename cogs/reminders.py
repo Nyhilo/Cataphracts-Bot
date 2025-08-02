509ca147b43d2a6533191f6d33b21f9751e84698
Nyhilo <jacclowa@gmail.com>
2025-08-02 11:24:26 -0700

    Formatting tweaks to reminder responses

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -189,10 +189,10 @@ class Reminders(commands.Cog, name='Reminders'):
                     log.info(unsetMsg)
                 continue
 
-            _msg = f'"{msg}"' if msg else ''
+            _msg = f'{msg}' if msg else ''
             try:
                 replyTo = await channel.fetch_message(task['MessageId'])
-                if task['Reoccur'] is None or task['Reoccur'] == 0:
+                if task['Reoccur']:
                     await replyTo.reply(locale.get_string('remindFound', userAt=userAt, message=_msg))
                 else:
                     await replyTo.reply(_msg)
@@ -201,7 +201,7 @@ class Reminders(commands.Cog, name='Reminders'):
             except discord.NotFound:
                 await replyTo.reply(locale.get_string('remindChannelNotFound',
                                                       userAt=userAt, createdAt=createdAt, message=_msg))
-            if task['Reoccur'] is None or task['Reoccur'] == 0:
+            if not task['Reoccur']:
                 log.info(reminders.unset_reminder(rowId, overrideId=True))
 
 
@@ -211,6 +211,8 @@ async def handle_set_reminder(ctx, userId, createdAt, messageId, channelId, remi
 
     if remindAfter.total_seconds() < 10:
         return await ctx.send(locale.get_string('remindSetTooShort'))
+
+    msg = msg.strip()
 
     if reoccur == Reoccur.NONE and reminders.can_quick_remind(remindAfter):
         seconds = remindAfter.total_seconds()
